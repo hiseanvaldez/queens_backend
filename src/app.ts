577068393d714a4f05cb3e3d80672bf5e13a331e
Shan Valdez <hisean.valdez.dev@gmail.com>
2025-07-26 00:55:56 +0800

    Add root endpoint with welcome message

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import { generatePuzzle } from "./lib/puzzle/generator";
 const app = express();
 
 app.get("/", (req, res) => {
+  res.send("Welcome to Queens API!");
+});
+
+app.get("/generate", (req, res) => {
   const size = parseInt(req.query.size as string) || 10;
   const puzzle = generatePuzzle(size);
 
