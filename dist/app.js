c129ceaf99d271ceca677556d3b4cc27fa61e146
Shan Valdez <hisean.valdez.dev@gmail.com>
2025-07-26 13:13:25 +0800

    Build

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const generator_1 = require("./lib/puzzle/generator");
+const corsOptions = {
+    origin: ["http://localhost:3000/", "https://react-sandbox-ebon.vercel.app/"],
+    optionsSuccessStatus: 200,
+};
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)(corsOptions));
 app.get("/", (req, res) => {
     res.send("Welcome to Queens API!");
 });
